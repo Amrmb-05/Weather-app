567d9a54eb9732bbeb8cb27f1f63c36d78a67af0
amrmohamed <amoree.mb@gmail.com>
2023-09-21 17:18:19 +0300

    Display current weather data

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ const search = document.querySelector("button");
 
 async function getData(location) {
   const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=cd729a38c2d0478696f121030231909&q=${location}&days=3`,
+    `https://api.weatherapi.com/v1/forecast.json?key=cd729a38c2d0478696f121030231909&q=${location}&days=4`,
     { mode: "cors" },
   );
   const weatherData = await response.json();
@@ -25,8 +25,12 @@ const weatherdata = getData("khartoum");
 async function parseCurrentWeather(data) {
   const response = await data;
   const currentWeatherData = {
+    city: response.location.name,
+    country: response.location.country,
+    condition: response.current.condition.text,
     currentTempC: response.current.temp_c,
     currentTempF: response.current.temp_f,
+    currentWindMph: response.current.wind_mph,
     feelsLikeC: response.current.feelslike_c,
     feelsLikeF: response.current.feelslike_f,
     maxTempC: response.forecast.forecastday[0].day.maxtemp_c,
@@ -41,31 +45,53 @@ async function parseCurrentWeather(data) {
 
 async function parseDailyWeather(data, day) {
   const response = await data;
-  const dailyWeatherData = {
-    avgTempC: response.forecast.forecastday[day].day.avgtemp_c,
-    avgTempF: response.forecast.forecastday[day].day.avgtemp_f,
-    maxTempC: response.forecast.forecastday[day].day.maxtemp_c,
-    maxTempF: response.forecast.forecastday[day].day.maxtemp_f,
-    minTempC: response.forecast.forecastday[day].day.mintemp_c,
-    minTempF: response.forecast.forecastday[day].day.mintemp_f,
-    chanceOfRain: response.forecast.forecastday[day].day.daily_chance_of_rain,
-  };
-  console.log(dailyWeatherData);
+  const forecastArr = [];
+  for (let i = 1; i <= day; i += 1) {
+    const dailyWeatherData = {
+      avgTempC: response.forecast.forecastday[i].day.avgtemp_c,
+      avgTempF: response.forecast.forecastday[i].day.avgtemp_f,
+      maxTempC: response.forecast.forecastday[i].day.maxtemp_c,
+      maxTempF: response.forecast.forecastday[i].day.maxtemp_f,
+      minTempC: response.forecast.forecastday[i].day.mintemp_c,
+      minTempF: response.forecast.forecastday[i].day.mintemp_f,
+      chanceOfRain: response.forecast.forecastday[i].day.daily_chance_of_rain,
+    };
+    forecastArr.push(dailyWeatherData);
+  }
+  console.log(forecastArr);
+}
+parseDailyWeather(weatherdata, 2);
+
+async function render(data) {
+  const main = document.getElementById("main");
+  const currentWeather = await parseCurrentWeather(data);
+  const forecast = await parseDailyWeather(data, 2);
+  main.innerHTML = "";
+  main.innerHTML = `
+  <div class="location">
+            <h2>${currentWeather.condition}</h2>
+            <h3>${currentWeather.city},  ${currentWeather.country}</h3>
+        </div>
+        <div class="today-weather">
+            <h1>${currentWeather.currentTempC}&deg;C</h1>
+            <div class="line"></div>
+            <ul>
+              <li>Feels like: ${currentWeather.feelsLikeC}&deg;C<li>
+              <li>Wind: ${currentWeather.currentWindMph} mph</li>
+              <li>Chance of rain: ${currentWeather.chanceOfRain}%</li>
+              <li> 
+              <ul>
+        <div class="forecast">
+            <h2>
+        </div> 
+        </div>
+  `;
 }
 
 search.addEventListener("click", () => {
   const location = input.value;
   console.log(location);
   const data = getData(location);
-  parseCurrentWeather(data);
-  parseDailyWeather(data, 1);
-  parseDailyWeather(data, 2);
+  render(data);
+  input.value = "";
 });
-// async function render(data) {
-//   const p = document.querySelector("p");
-//   const temp = await parseCurrentWeather(data);
-//   console.log(temp);
-//   p.innerText = temp.currentTempC;
-// }
-
-// render(weatherdata);
